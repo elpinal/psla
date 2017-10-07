6ae50ca13742013b97e81d92ca4c6a8692be86ed
elpinal <elpinal@users.noreply.github.com>
2017-10-07 22:36:03 +0900

    Add test for parseFlag

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -7,10 +7,23 @@ import Control.Monad.State.Lazy
 import Lib
 
 spec :: Spec
-spec =
+spec = do
   describe "installFlags" $
     it "parses one or no flag in args" $ do
       let xs = ["-config", "conf", "-framework", "arg1", "arg2"]
       let (flag, args) = runState installFlags xs
       flag `shouldBe` Just (Config "conf")
       args `shouldBe` ["-framework", "arg1", "arg2"]
+
+  describe "parseFlag" $ do
+    it "returns arguments as it is if no flag are given" $ do
+      let xs = ["arg"]
+      let (flag, args) = runState (parseFlag installFlags) xs
+      flag `shouldBe` []
+      args `shouldBe` xs
+
+    it "parses flags in args" $ do
+      let xs = ["-config", "conf", "-framework", "arg1", "arg2"]
+      let (flag, args) = runState (parseFlag installFlags) xs
+      flag `shouldBe` [Config "conf", Framework]
+      args `shouldBe` ["arg1", "arg2"]
